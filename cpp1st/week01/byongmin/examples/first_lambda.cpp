3d1cce776a1d090c3d46b004bc362afc5d4765eb
ByungMin <fkrocks@gmail.com>
2022-03-10 01:09:55 +0900

    first lambda example
    first lambda example
    https://docs.microsoft.com/en-us/cpp/cpp/examples-of-lambda-expressions?view=msvc-170

---
+++ cpp1st/week01/byongmin/examples/first_lambda.cpp
@@ -0,0 +1,22 @@
+#include <functional>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    // Assign the lambda expression that adds two numbers to an auto variable.
+    auto f1 = [](int x, int y) { return x + y; };
+
+    cout << f1(2, 3) << endl;
+
+    // Assign the same lambda expression to a function object.
+    function<int(int, int)> f2 = [](int x, int y) { return x + y; };
+
+    cout << f2(3, 4) << endl;
+}
+
+// output 
+// ----------------
+// 5
+// 7
