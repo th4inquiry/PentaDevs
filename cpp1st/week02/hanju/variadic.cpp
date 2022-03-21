85316113fead2bc554aa43c4dffa8f95b1dda458
H <h.public.learning@gmail.com>
2022-03-21 22:23:38 +0900

    variadic

---
+++ cpp1st/week02/hanju/variadic.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std::string_literals; // enables s-suffix for std::string literals
+
+class VariadicTest
+{
+public:
+    void testAll();
+protected:
+    template <typename T>
+    T add(T p)
+    {
+        return p;
+    }
+
+    template <typename T, typename... Ts>
+    T add(T first, Ts... others)
+    {
+        return first + add(others...);
+    }
+};
+
+void VariadicTest::testAll()
+{
+    auto a = add(1,2,3,4,5,6,7,8,9,10);
+    auto s = add("this"s," "s,"is"s," a "s,"test"s);
+
+    std::cout << a << std::endl;
+    std::cout << s << std::endl;
+}
+
+int main()
+{
+    VariadicTest tester;
+    tester.testAll();
+
+    return 0;
+}
